557b8d64d1d5fa8248b7ee0cb941ddbe37019217
epicmeaw <luandy108@gmail.com>
2024-03-05 20:29:13 -0500

    adapt with the new information from the backend
    Change it so it get the information from the backend and then creates the tabs from it

--- side_panel/side_panel.ts
+++ side_panel/side_panel.ts
@@ -10,24 +10,23 @@ interface Window {
     window.WINDOW_ID = wid;
 
     window.tabPort = chrome.runtime.connect({ name: `${wid}` });
-    window.tabPort.onMessage.addListener((message: S2CMessage) => {
-        console.log(message)
+    window.tabPort.onMessage.addListener((message: S2CMessage | string []) => {
+        if(Array.isArray(message)){
+            let tabLength = message.length
+            for(let i = 0; i < tabLength; i++){
+                addElement(message[i]);
+            }
+        }
     });
 })().catch(console.error);
 
-// Get the button element
-const button = document.getElementById("clickMe");
-
-// Add event listener to the button
-button.addEventListener("click", addElement);
-
-function addElement() {
+function addElement(Names: string) {
     // Creating newTab with the element of div
     const newTab = document.createElement("div");
     //Makind the newTab to have a class
     newTab.classList.add("Tabs")
     // Making tabcontent to contain text
-    const tabContent = document.createTextNode("test");
+    const tabContent = document.createTextNode(Names);
     // Make newtab have tabContent
     newTab.appendChild(tabContent);
     // Get the main container element
