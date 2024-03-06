86c96dc377b44297674a02c345cd9a927eea4616
epicmeaw <luandy108@gmail.com>
2024-03-05 19:38:54 -0500

    Created Tabs and abilities to insert new tabs
    allow tabs to be able to fix on the screen and create the abilities to add more tabs.

--- side_panel/side_panel.ts
+++ side_panel/side_panel.ts
@@ -14,3 +14,24 @@ interface Window {
         console.log(message)
     });
 })().catch(console.error);
+
+// Get the button element
+const button = document.getElementById("clickMe");
+
+// Add event listener to the button
+button.addEventListener("click", addElement);
+
+function addElement() {
+    // Creating newTab with the element of div
+    const newTab = document.createElement("div");
+    //Makind the newTab to have a class
+    newTab.classList.add("Tabs")
+    // Making tabcontent to contain text
+    const tabContent = document.createTextNode("test");
+    // Make newtab have tabContent
+    newTab.appendChild(tabContent);
+    // Get the main container element
+    const mainContainer = document.querySelector(".MainContainer");
+    // Append newTab to the main container
+    mainContainer.appendChild(newTab);
+}
